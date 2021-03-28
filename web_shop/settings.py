fbc534cde976514864eab50449f9e1610db022f1
sergey <sergeyYH@yandex.ru>
2021-03-28 15:55:56 +0300

    db migrations

--- web_shop/settings.py
+++ web_shop/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'web_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'u1305036_default',
+        'USER': 'u1305036_default',
+        'PASSWORD': 'iO3mMj!F',
+        'HOST': 'localhost',
     }
 }
 
